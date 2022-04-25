ce80bc3a1f2bb0ccf047b93e229a776f201df533
Moise Tshiibwabwa <71385862+TshiMoiset@users.noreply.github.com>
2022-04-25 12:44:46 +0200

    Class: Menu

--- Slutprojektet/Menu.cs
+++ Slutprojektet/Menu.cs
@@ -16,26 +16,13 @@ namespace Slutprojektet
             // Loopen behövs för att menyn ska skrivas ut. Den ser också till att skriva ut menyn en
             while (menuChoises != 1)    // menuChoises != 1 gör så att kod blocket körs en gång när man trycker på val 1. 
             {
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Välkommen till Tamagotchi");
-                Console.WriteLine();
-                Console.WriteLine("Välj alternativ 1 eller 2 och tryck [ENTER] för att fortsätta.");
-                Console.WriteLine("1. Starta spel!");
-                Console.WriteLine("2. Hur funkar det?");
-                Console.WriteLine();
+                menuAlternatives();
                 menuChoisesString = Console.ReadLine();
 
                 // Gör om string till en int går inte vidare i spelet om man inte svara utifrån vad programmet frågar efter.
                 while (!int.TryParse(menuChoisesString, out menuChoises))
                 {
-                    Console.WriteLine();
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Det där är inte ett giltigt svar!");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine("Välj 1 eller 2, Försök igen!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("Okej, Jag väljer då: ");
+                    wrongMenuInput();
                     menuChoisesString = Console.ReadLine();
                 }
 
@@ -48,18 +35,44 @@ namespace Slutprojektet
                 // Om man svarar 2 får man en förklaring till hur spelt funkar. 
                 else if (menuChoisesString == "2")
                 {
-                    Console.Clear();
-                    Console.WriteLine("Spelet går ut på att hålla din Tamagotchi vid liv.");
-                    Console.WriteLine("Du ska välja mellan att lära den ett nytt ord, hälsa på den, mata den eller göra ingenting.");
-                    Console.WriteLine("Varje gång du gör ett val ökar hunger och boredom, och om någon av de blir över 10 så DÖR din tamagotchi.");
-                    Console.WriteLine("DÖD TAMAGOTCHI = GAME OVER");
-                    Console.WriteLine("Lycka till!");
+                    gameInformation();
                 }
 
                 Console.WriteLine();
             }
+        }
 
-            Console.ReadLine();
+        void menuAlternatives()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Välkommen till Tamagotchi");
+            Console.WriteLine();
+            Console.WriteLine("Välj alternativ 1 eller 2 och tryck [ENTER] för att fortsätta.");
+            Console.WriteLine("1. Starta spel!");
+            Console.WriteLine("2. Hur funkar det?");
+            Console.WriteLine();
+        }
+
+        void wrongMenuInput()
+        {
+            Console.WriteLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Det där är inte ett giltigt svar!");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Välj 1 eller 2, Försök igen!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Okej, Jag väljer då: ");
+        }
+
+        void gameInformation()
+        {
+            Console.Clear();
+            Console.WriteLine("Spelet går ut på att hålla din Tamagotchi vid liv genom att");
+            Console.WriteLine("välja mellan att lära den ett nytt ord, hälsa på den, mata den eller göra ingenting.");
+            Console.WriteLine("Varje gång du gör ett val ökar hunger och boredom eller så sjunker de, och om någon av de blir över 10 så DÖR din Tamagotchi.");
+            Console.WriteLine("DÖD TAMAGOTCHI = GAME OVER");
+            Console.WriteLine("Lycka till!");
         }
     }
 }
